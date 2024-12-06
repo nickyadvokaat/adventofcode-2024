5adbba164e6178169a5c926250ce06628d548515
Nicky Advokaat <nickyadvokaat@users.noreply.github.com>
2024-12-06 15:22:02 +0100

    feat: day06 - boolean grid

--- src/day06.ts
+++ src/day06.ts
@@ -7,11 +7,14 @@ type MoveHistoryItem = { direction: Direction; position: Coord }
 
 export default function day06() {
   const data = readFile('06').map((s) => s.split(''))
-  const grid = new Grid(data)
+  const tempGrid = new Grid(data)
+  tempGrid.transpose()
+  const startPosition = tempGrid.find('^')[0]
+
+  const grid = new Grid(data.map((a) => a.map((s) => s === '#')))
   grid.transpose()
 
-  const { moveHistory } = runGuardPath(grid)
-
+  const { moveHistory } = runGuardPath(grid, startPosition)
   const set = new Set<string>()
   moveHistory
     .map((m) => m.position)
@@ -19,7 +22,6 @@ export default function day06() {
     .forEach((s) => set.add(s))
   console.log(set.size)
 
-  const startPosition = grid.find('^')[0]
   let possibleObjectLocations: Coord[] = []
   set.forEach((s) => {
     const split = s.split('-')
@@ -33,25 +35,26 @@ export default function day06() {
   )
 
   const solutionPart2 = possibleObjectLocations.filter((c) => {
-    grid.set(c, '#')
-    const { didLoop } = runGuardPath(grid)
-    grid.set(c, '.')
+    grid.set(c, true)
+    const { didLoop } = runGuardPath(grid, startPosition)
+    grid.set(c, false)
     return didLoop
   }).length
   console.log(solutionPart2)
 }
 
-function runGuardPath(grid: Grid<string>): {
+function runGuardPath(
+  grid: Grid<boolean>,
+  position: Coord
+): {
   moveHistory: MoveHistoryItem[]
   didLoop: boolean
 } {
   let direction: Direction = Direction.N
-  let position = grid.find('^')[0]
   let moveHistory: MoveHistoryItem[] = []
-  let done = false
-  while (!done) {
+  while (true) {
     moveHistory.push({ direction: direction, position: position })
-    if (grid.getInDirection(position, direction) === '#') {
+    if (grid.getInDirection(position, direction) === true) {
       direction = DirectionToRight(direction)
     } else {
       position = getCoordInDirection(position, direction)
@@ -65,7 +68,7 @@ function runGuardPath(grid: Grid<string>): {
           item.direction === direction
       )
     ) {
-      done = true
+      break
     }
   }
   return { didLoop: !grid.isOutOfBounds(position), moveHistory }
