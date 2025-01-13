a4f4705d0560fcaa5e3cb344882c40febaa25bc4
Nicky Advokaat <nickyadvokaat@users.noreply.github.com>
2025-01-13 15:01:03 +0100

    feat: day19 part 2

--- src/day19.spec.ts
+++ src/day19.spec.ts
@@ -1,23 +1,32 @@
 import { describe, expect, test } from 'vitest'
-import { isDesignPossible } from './day19'
+import { isDesignPossible, numberOfDesignOptions } from './day19'
 
 describe('Day19', () => {
+  const options = ['r', 'wr', 'b', 'g', 'bwu', 'rb', 'gb', 'br']
+  const towels = [
+    'brwrr',
+    'bggr',
+    'gbbr',
+    'rrbgbr',
+    'ubwu',
+    'bwurrg',
+    'brgr',
+    'bbrgwb',
+  ]
+  const maxLength = options.reduce((a, c) => Math.max(a, c.length), 0)
+
   test('Part 1', () => {
-    const options = ['r', 'wr', 'b', 'g', 'bwu', 'rb', 'gb', 'br']
-    const towels = [
-      'brwrr',
-      'bggr',
-      'gbbr',
-      'rrbgbr',
-      'ubwu',
-      'bwurrg',
-      'brgr',
-      'bbrgwb',
-    ]
-    const maxLength = options.reduce((a, c) => Math.max(a, c.length), 0)
     const part1 = towels.filter((t) =>
       isDesignPossible(t, options, maxLength)
     ).length
     expect(part1).toBe(6)
+  })
+
+  test('Part 2', () => {
+    const part2 = towels.reduce(
+      (a, c) => a + numberOfDesignOptions(c, options, maxLength),
+      0
+    )
+    expect(part2).toBe(16)
   })
 })
--- src/day19.ts
+++ src/day19.ts
@@ -1,16 +1,19 @@
 import { readFile } from './util/fileUtil'
 
 export default function day19() {
-  const data = readFile('19-t')
+  const data = readFile('19')
   const options = data.filter((s) => s.includes(','))[0].split(', ')
   const towels = data.filter((s) => !s.includes(',') && s.length > 0)
-
-  console.log(options, towels)
   const maxLength = options.reduce((a, c) => Math.max(a, c.length), 0)
   const part1 = towels.filter((t) =>
     isDesignPossible(t, options, maxLength)
   ).length
   console.log(part1)
+  const part2 = towels.reduce(
+    (a, c) => a + numberOfDesignOptions(c, options, maxLength),
+    0
+  )
+  console.log(part2)
 }
 
 export function isDesignPossible(
@@ -19,7 +22,6 @@ export function isDesignPossible(
   maxLength: number,
   index = 0
 ): boolean {
-  // console.log(t.slice(0, index - 1), index === t.length)
   if (index === t.length) return true
   for (let i = 1; i <= Math.min(maxLength, t.length - index); i++) {
     const sub = t.slice(index, index + i)
@@ -28,4 +30,30 @@ export function isDesignPossible(
     }
   }
   return false
+}
+
+export function numberOfDesignOptions(
+  t: string,
+  options: string[],
+  maxLength: number,
+  index = 0,
+  mem = new Map<string, number>()
+): number {
+  if (index === t.length) {
+    return 1
+  }
+  let result = 0
+  for (let i = 1; i <= Math.min(maxLength, t.length - index); i++) {
+    if (options.some((o) => o === t.slice(index, index + i))) {
+      const key = t.slice(index + i, t.length)
+      if (mem.has(key)) {
+        result += mem.get(key)!
+      } else {
+        let n = numberOfDesignOptions(t, options, maxLength, index + i, mem)
+        mem.set(key, n)
+        result += n
+      }
+    }
+  }
+  return result
 }
